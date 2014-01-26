90570fe4cdc6f3d012fd81adfc2fe55087f6865a
katya <istepan@yandex.ru>
2014-01-26 23:52:42 +0600

    add files

---
+++ dl_html_users/lang/English/dl_html_users.php
@@ -0,0 +1,8 @@
+<?php
+$lang_dl_html_users = array(
+    'HTML code' => 'HTML code',
+    'HTML code displayed in the block author' => 'HTML code displayed in the block author',
+    'Allow the user to change the code? Attention! It\'s not safe.' => 'Allow the user to change the code? <b>Attention! It\'s not safe.</b>',
+    'Display HTML to author message block?' => 'Display HTML to author message block?',
+    'Preview HTML' => 'Preview HTML'
+);
---
+++ dl_html_users/lang/Russian/dl_html_users.php
@@ -0,0 +1,8 @@
+<?php
+$lang_dl_html_users = array(
+    'HTML code' => 'HTML код',
+    'HTML code displayed in the block author' => 'HTML код, отображаемый в блоке автора сообщения',
+    'Allow the user to change the code? Attention! It\'s not safe.' => 'Разрешить пользователю редактировать код самому? <b>Внимание! Это не безопасно.</b>',
+    'Display HTML to author message block?' => 'Включить отображение HTML в блоках автора сообщения?',
+    'Preview HTML' => 'Предпросмотр HTML'
+);
